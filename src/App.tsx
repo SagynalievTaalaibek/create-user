151d5e6c21b122676c667ac1d367db748377995b
Taalaibek Sagynaliev <sagynalievv.t@gmail.com>
2023-11-08 18:36:37 +0600

    add component Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import Navbar from './components/Navbar/Navbar';
+import UserForm from './components/UserForm/UserForm';
+import Users from './components/Users/Users';
+
 const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <header className="bg-success">
+        <Navbar/>
+      </header>
+      <main className="container">
+        <div className="row mt-3">
+          <div className="col-6">
+            <UserForm/>
+          </div>
+          <div className="col-6">
+            <Users/>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <a className="navbar-brand" href="#">Users</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
